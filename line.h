c3c449e678fd988061d5c46f008fb65425f4886b
DTDHZM <56240771+DTDHZM@users.noreply.github.com>
2025-05-09 19:14:22 +0800

    Update line.h

--- line.h
+++ line.h
@@ -2,11 +2,21 @@
 #define LINE_H
 
 #include "shape.h"
+#include <QPoint>
 
 class Line : public Shape
 {
 public:
     void paint(QPainter &painter) override;
+    QString toString() const override; // 实现 toString 方法
+    void fromString(const QString &data) override; // 实现 fromString 方法
+
+    void setStart(const QPoint &point) { start = point; }
+    void setEnd(const QPoint &point) { end = point; }
+
+private:
+    QPoint start;
+    QPoint end;
 };
 
 #endif // LINE_H
