85eec1b4de273a57a38e2c01425dff7b35e6b6cb
DTDHZM <56240771+DTDHZM@users.noreply.github.com>
2025-05-09 19:15:26 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,28 +1,43 @@
-﻿#ifndef MAINWINDOW_H
+#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include "ui_mainwindow.h"
 
 #include <QMainWindow>
-#include <QToolBar>
-#include <QActionGroup>
-#include "paintwidget.h"
+#include <QPoint>
+#include <QPainter>
+#include <vector>
+#include "line.h"
+#include "rect.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-signals:
-    void changeCurrentShape(Shape::Code newShape);
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 private slots:
-    void drawLineActionTriggered();
-    void drawRectActionTriggered();
+    void saveShapesToFile();
+    void loadShapesFromFile();
+    void selectDrawLine();
+    void selectDrawRectangle();
 
 private:
     Ui::MainWindow *ui;
-    PaintWidget *paintWidget;
+    std::vector<Shape*> shapes; // 存储所有形状
+    QPoint startPoint; // 起点
+    QPoint endPoint;   // 终点
+    bool isDrawing;    // 是否正在绘制
+    enum DrawMode { None, LineMode, RectMode } drawMode; // 绘图模式
 };
 
 #endif // MAINWINDOW_H
