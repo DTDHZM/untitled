3fe546bfd48ad7011c225e98778ea0279895c9f6
DTDHZM <56240771+DTDHZM@users.noreply.github.com>
2025-05-09 19:16:48 +0800

    Update rect.cpp

--- rect.cpp
+++ rect.cpp
@@ -3,3 +3,19 @@
 void Rect::paint(QPainter &painter) {
     painter.drawRect(QRect(start, end));
 }
+
+QString Rect::toString() const {
+    return QString("Rect,%1,%2,%3,%4")
+    .arg(start.x()).arg(start.y())
+        .arg(end.x()).arg(end.y());
+}
+
+void Rect::fromString(const QString &data) {
+    auto parts = data.split(',');
+    if (parts.size() == 5 && parts[0] == "Rect") {
+        start.setX(parts[1].toInt());
+        start.setY(parts[2].toInt());
+        end.setX(parts[3].toInt());
+        end.setY(parts[4].toInt());
+    }
+}
